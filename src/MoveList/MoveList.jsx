6bad237c225e9fbdd758664239c1d27f097fbc01
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-10-20 19:32:08 +0300

    next button style and refactor list title

--- src/MoveList/MoveList.jsx
+++ src/MoveList/MoveList.jsx
@@ -2,27 +2,34 @@ import React from 'react';
 
 import './MoveList.scss';
 
-export const MoveList = ({ moves, onRemoveMove, allFilled, handleNextLevel }) => {
+export const MoveList = ({
+  moves,
+  onRemoveMove,
+  allFilled,
+  handleNextLevel,
+}) => {
   return (
     <div className="move-list">
-      <h1 className="title">Hover Squares</h1>
-
       {moves.length > 0 && !allFilled && (
-        <ul className="list">
-          {moves.map((move) => (
-            <li key={move} className="list-item">
-              <div className="item-title">{move}</div>
-              <button
-                className="delete is-small"
-                onClick={() => onRemoveMove(move)}
-              >
-                x
-              </button>
-            </li>
-          ))}
-        </ul>
+        <>
+          <h1 className="title">Hover Squares</h1>
+          
+          <ul className="list">
+            {moves.map((move) => (
+              <li key={move} className="list-item">
+                <div className="item-title">{move}</div>
+                <button
+                  className="delete is-small"
+                  onClick={() => onRemoveMove(move)}
+                >
+                  x
+                </button>
+              </li>
+            ))}
+          </ul>
+        </>
       )}
-      
+
       {moves.length === 0 && (
         <h2 className="title-start">
           To begin, hover your cursor over any square
@@ -31,17 +38,17 @@ export const MoveList = ({ moves, onRemoveMove, allFilled, handleNextLevel }) =>
 
       {allFilled && (
         <>
-        <h2>Welcome!</h2>
-        <h2 className="title-start">
-          All cells are filled. You can proceed to the next level.
-        </h2>
-        <button
-                type="button"
-                className="next-level-button"
-                onClick={handleNextLevel}
-              >
-                Next
-              </button>
+          <h2 className="title-congrats">Welcome!</h2>
+          <h2 className="title-next">
+            All cells are filled. You can proceed to the next level.
+          </h2>
+          <button
+            type="button"
+            className="next-level-button"
+            onClick={handleNextLevel}
+          >
+            Next
+          </button>
         </>
       )}
     </div>
